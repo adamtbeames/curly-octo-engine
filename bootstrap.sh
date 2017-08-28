d74ba623b49d27e5b9b1472475f1608554faa9e5
Adam Beames <adamtbeames@gmail.com>
2017-08-28 14:08:25 +0000

    Cleanup

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
 apt-get install git vim jq
+
+echo "<IfModule mod_dir.c>\n\tDirectoryIndex index.php index.html index.cgi index.pl index.xhtml index.htm\n</IfModule>">/etc/apache2/mods-enabled/dir.conf
