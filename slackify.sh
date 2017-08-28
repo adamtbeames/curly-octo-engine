aa0ebb0cebc8400824d0c281bf2f524ee1eb1f22
Adam Beames <adamtbeames@gmail.com>
2017-08-28 13:41:09 +0000

    Add main slackify script

---
+++ slackify.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+config=$1
+
+# Function to deal with packages
+function config_apt {
+  # Get packages from config
+  apt=`cat $config | jq '.apt'| grep -ve \\\[ -ve \\\] | sed 's/,//g' | sed 's/"//g'`
+  # Install packages 
+  apt-get install $apt -y
+}
+
+# Function to deal with files
+function config_file {
+  # Get file attributes from config
+  filesource=`cat $config | jq '.file.source' | sed 's/"//g'`
+  filedest=`cat $config | jq '.file.dest' | sed 's/"//g'`
+  fileuser=`cat $config | jq '.file.user' | sed 's/"//g'`
+  filegroup=`cat $config | jq '.file.group' | sed 's/"//g'`
+  fileperms=`cat $config | jq '.file.perms' | sed 's/"//g'`
+  # Check for existing user and group  before adding to keep console cleam
+  if ! genent passwd $fileuser > /dev/null 2>&1
+  then
+    useradd $fileuser 2>/dev/null
+  fi
+  if ! getent group $filegroup > /dev/null 2>&1
+  then
+    groupadd $filegroup >/dev/null
+  fi
+  # Deploy file
+  cp $filesource $filedest
+  # Set file owner
+  chown $fileuser:$filegroup $filedest
+  # Set file permissions
+  chmod $fileperms $filedest
+}
+
+# Function to deal with service
+function config_service {
+  # Get service from config
+  service=`cat $config | jq '.service' | sed 's/"//g'`
+  # Restart service
+  service $service restart
+}
+
+config_apt
+config_file
+config_service
